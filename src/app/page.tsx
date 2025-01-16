a8d9c2c0d5757c05c11e1233c63893af2b297da2
“Vishal <vishaldevkate1981@gmail.com>
2025-01-16 17:37:11 +0530

    featured courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import FeaturedCourses from '@/components/FeaturedCourses'
 import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
 export default function page() {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
-      <div className='text-center'>page</div>
+      <div className='text-center'></div>
       <HeroSection/>
+      <FeaturedCourses/>
     </main>
   )
 }
